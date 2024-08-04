67d5f36c2e44e68db3ac72bd572cef1e049cb1fa
cyh <1909625265@qq.com>
2024-08-05 02:36:13 +0800

    fix base

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -3,12 +3,13 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/course",
 
   lang: "zh-CN",
   title: "文档",
   description: "cyh的文档",
 
+  
   theme,
 
   // Enable it with pwa
